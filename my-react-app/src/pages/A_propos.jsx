c6d4974526a957119ad4d67c061831e2fe09fb23
Julien Auclair <juju.auclair@gmail.com>
2025-01-31 03:11:15 +0100

    version_finale

--- my-react-app/src/pages/A_propos.jsx
+++ my-react-app/src/pages/A_propos.jsx
@@ -11,7 +11,7 @@ const Apropos = () => {
     const ListeDescription = ['Les annonces postés sur Kasa garanrissent une fiabilité totale. Les Photos sont conformes aux logements, et toutes les informations sont régulièrement véfifiées par nos équipes.',
         'La bienveillance fait partie des valeurs fondatrices che Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme',
         'La qualité du service est au coeur de notre engagement che Kasa. Nous veillons à ce que chaque interaction soit empreinte de respect et de bienveillance.',
-        'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l/hôte qu/au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés']
+        'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés.']
     return (
         <div>
             <Header />
